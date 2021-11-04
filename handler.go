acf7d3a93114a19f644357e379ad82446c8b8c7c
rafael.reis <rafael.reis@sistemapoliedro.com.br>
2021-11-04 09:49:53 -0300

    creating menu

--- handler.go
+++ handler.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Word!")
+	name := "Rafael"
+	var version float32 = 1.1
+
+	fmt.Println("Name: ", name)
+	fmt.Println("Version: ", version)
+
+	fmt.Println("1- Monitoring Start")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit")
 }
