98873e5051e6fc2f07a14267f6f669150c933b45
rafael.reis <rafael.reis@sistemapoliedro.com.br>
2021-11-04 13:57:41 -0300

    first refactpring

--- handler.go
+++ handler.go
@@ -2,22 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	showIntroduction()
+	showMenu()
+	command := readCommand()
+
+	switch command {
+	case 1:
+		fmt.Println("Monitoring ...")
+	case 2:
+		fmt.Println("Logging ...")
+	case 0:
+		fmt.Println("Exiting ...")
+		os.Exit(0)
+	default:
+		fmt.Println("Command not found!")
+		os.Exit(-1)
+	}
+}
+
+func showIntroduction() {
 	name := "Rafael"
 	var version float32 = 1.1
 
 	fmt.Println("Name: ", name)
 	fmt.Println("Version: ", version)
+}
 
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+	fmt.Println("The command selected was ", command)
+
+	return command
+}
+
+func showMenu() {
 	fmt.Println("1- Monitoring Start")
 	fmt.Println("2- Show Logs")
 	fmt.Println("0- Exit")
-
-	var command int
-
-	fmt.Scan(&command)
-
-	fmt.Println("The command selected was ", command)
 }
